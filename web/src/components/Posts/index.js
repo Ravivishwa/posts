a59e132126bac85bef128858ba18e60240365e56
ravivishwa <raviv.vishwakarma@gmail.com>
2019-12-30 23:59:09 +0530

    updates

--- web/src/components/Posts/index.js
+++ web/src/components/Posts/index.js
@@ -1,33 +1,45 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography,ListItem } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
+import {posts} from "../../dummy-post";
+import {
+  AutoSizer,
+  List,
+  CellMeasurer,
+  CellMeasurerCache,
+} from 'react-virtualized';
 
-function Posts(props) {
-    const { posts } = props
-  return (
-    <div style={{ padding: 30 }}>
-      <Grid container spacing={6} justify="center">
-        {posts && posts.map(post => (
-          <Grid item key={post.description} xs={3}>
-            <Card>
+export default class Posts extends React.Component {
+
+
+  renderRow = ({ index, isScrolling, key, style}) => {
+    console.log(this.props)
+    const { posts } =   this.props
+    return (
+      <div key={key} style={style}>
+        {/* <div>{this.props.posts[index].description}</div> */}
+        {/* <div>{this.props.data[index].email}</div> */}
+        <Card>
               <CardActionArea>
                 <CardMedia
                   component="img"
                   alt="Contemplative Reptile"
                   height="140"
-                  image={post.data.media[0].image}
+                  image={posts[index].data.media[0].image}
+                  // image={post.image}
                   title="Contemplative Reptile"
                 />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="h2">
-                    {post.data.media[0].description}
+                    {/* {post.data.media[0].description} */}
+                    {/* {post.description} */}
                   </Typography>
-                  <Typography component="p">{post.description}</Typography>
+                  {/* <Typography component="p">{post.excerpt}</Typography> */}
                 </CardContent>
               </CardActionArea>
               <CardActions>
@@ -39,11 +51,60 @@ function Posts(props) {
                 </Button>
               </CardActions>
             </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </div>
-  );
+      </div>
+    )
+  }
+
+
+  render() {
+    return (
+    <List
+            rowCount={this.props.posts.length}
+            rowHeight={50} width={300} height={300}
+            rowRenderer={this.renderRow}
+          />        
+    );
+  }
 }
 
-export default Posts;
+// function Posts(props) {
+//     // const { posts } = props
+//   return (
+//     <div style={{ padding: 30 }}>
+//       <List container spacing={6} justify="center">
+//         {posts && posts.map((post,i) => (
+//           <ListItem key={i} xs={3}>
+            // <Card>
+            //   <CardActionArea>
+            //     <CardMedia
+            //       component="img"
+            //       alt="Contemplative Reptile"
+            //       height="140"
+            //       // image={post.data.media[0].image}
+            //       image={post.image}
+            //       title="Contemplative Reptile"
+            //     />
+            //     <CardContent>
+            //       <Typography gutterBottom variant="h5" component="h2">
+            //         {/* {post.data.media[0].description} */}
+            //         {post.description}
+            //       </Typography>
+            //       <Typography component="p">{post.excerpt}</Typography>
+            //     </CardContent>
+            //   </CardActionArea>
+            //   <CardActions>
+            //     <Button size="small" color="primary">
+            //       Share
+            //     </Button>
+            //     <Button size="small" color="primary">
+            //       Learn More
+            //     </Button>
+            //   </CardActions>
+            // </Card>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </div>
+//   );
+// }
+
--- web/src/dummy-post.js
+++ web/src/dummy-post.js
@@ -35,4 +35,314 @@ export const posts = [
      excerpt: "This is my sixth post with more content inside",
      image: "https://bit.ly/2WNi2Ml"
     }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
+    ,
+   
+    {
+     title: "My second post",
+     excerpt: "This is my second post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My third post",
+     excerpt: "This is my third post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fourth post",
+     excerpt: "This is my fourth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My fifth post",
+     excerpt: "This is my fifth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    },
+   
+    {
+     title: "My sixth post",
+     excerpt: "This is my sixth post with more content inside",
+     image: "https://bit.ly/2WNi2Ml"
+    }
    ]
